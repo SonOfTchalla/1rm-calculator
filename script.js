88163f544397a370f9f96139f18d835aa1a3d97b
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2025-02-25 19:28:29 +0200

    Correct formula

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let calcEL = document.getElementById("calc");
 function calculateMax(weight, reps){
     // calculating max using the Bryzycki equation 
     // (Brzycki, M. (1993). Strength Testing—Predicting a One-Rep Max from Reps-to-Fatigue. Journal of Physical Education, Recreation & Dance, 64(1), 88–90. https://doi.org/10.1080/07303084.1993.10606684)
-    return weight / (1.0278+0.0278*reps)
+    return weight / (1.0278 - 0.0278 * reps)
 }
 
 calcEL.addEventListener('click', function(){
@@ -15,5 +15,5 @@ calcEL.addEventListener('click', function(){
     let reps = repsEL.value;
     let result = calculateMax(weight, reps);
 
-    console.log(result);
+    
 })
