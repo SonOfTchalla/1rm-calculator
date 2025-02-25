251fbec27fe50a397b7743083a50d7af4d880bfd
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2025-02-25 19:21:50 +0200

    Add function to calculate max

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 let numEL = document.getElementById("num");
 let repsEL = document.getElementById("reps");
 let calcEL = document.getElementById("calc");
+
+//function to calculate 1RM
+function calculateMax(weight, reps){
+    //calculating max using the Bryzycki equation 
+    // (Brzycki, M. (1993). Strength Testing—Predicting a One-Rep Max from Reps-to-Fatigue. Journal of Physical Education, Recreation & Dance, 64(1), 88–90. https://doi.org/10.1080/07303084.1993.10606684)
+    return weight / (1.0278+0.0278*reps)
+}
