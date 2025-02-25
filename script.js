19d18c0e3f492b1b83f41468216ef364bc2ab8de
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2025-02-25 19:26:59 +0200

    Add click event listener for button

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-//Query Selectors
+// Query Selectors
 let numEL = document.getElementById("num");
 let repsEL = document.getElementById("reps");
 let calcEL = document.getElementById("calc");
 
-//function to calculate 1RM
+// function to calculate 1RM
 function calculateMax(weight, reps){
-    //calculating max using the Bryzycki equation 
+    // calculating max using the Bryzycki equation 
     // (Brzycki, M. (1993). Strength Testing—Predicting a One-Rep Max from Reps-to-Fatigue. Journal of Physical Education, Recreation & Dance, 64(1), 88–90. https://doi.org/10.1080/07303084.1993.10606684)
     return weight / (1.0278+0.0278*reps)
 }
+
+calcEL.addEventListener('click', function(){
+    let weight = numEL.value;
+    let reps = repsEL.value;
+    let result = calculateMax(weight, reps);
+
+    console.log(result);
+})
